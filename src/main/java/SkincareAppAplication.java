23f7ffb3979f6c2cc6a9d9b5c1c3cab8816722a9
= <joridalilollari27@gamil.com>
2024-05-26 11:40:05 +0200

    creatingMavenPom

---
+++ src/main/java/SkincareAppAplication.java
@@ -0,0 +1,11 @@
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+
+public class SkincareAppAplication {
+    public static void main(String[] args) {
+        var context = SpringApplication.run(SkincareAppAplication.class, args);
+
+    }
+}
