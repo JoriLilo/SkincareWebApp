d41bfed2541b7d53f3aedf3edb8fa08741d1fd70
= <joridalilollari27@gamil.com>
2024-05-26 14:59:40 +0200

    product-entity

--- src/main/java/SkincareWebApp/entities/Product.java
+++ src/main/java/SkincareWebApp/entities/Product.java
@@ -2,10 +2,11 @@ package SkincareWebApp.entities;
 
 
 import SkincareWebApp.model.SkinType;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 
+import javax.annotation.processing.Generated;
 import java.util.Date;
 
 @Entity
@@ -13,14 +14,30 @@ import java.util.Date;
 
 public class Product {
 
-
+        @Id
+        @Column(name = "PRODUCT_ID")
+        @GeneratedValue(strategy = GenerationType.AUTO)
         private Long Id;
+        @Column(name = "NAME")
+        @NotNull
         private String name;
+
+        @Column(name = "DESCRIPTION")
         private String description;
+
+        @Column(name = "PRICE")
         private double price;
+
+        @Column(name = "STOCK")
         private Integer stock;
+
+        @Column(name = "EXPIRY_DATE")
         private Date expiryDate;
+
+        @Column(name = "QUANTITY")
         private Integer quantity;
+
+        @Column(name = "SKIN_TYPE")
         private SkinType skinType;
 
 
