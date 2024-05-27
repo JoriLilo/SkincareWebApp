b8ed06ffe9072881e3f616edbe37c00531117a8a
= <joridalilollari27@gamil.com>
2024-05-27 22:52:37 +0200

    productModel

--- src/main/java/SkincareWebApp/model/ProductModel.java
+++ src/main/java/SkincareWebApp/model/ProductModel.java
@@ -1,4 +1,32 @@
 package SkincareWebApp.model;
 
+import jakarta.persistence.Column;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class ProductModel {
+
+    private Long Id;
+
+    @NotNull
+    private String name;
+    private String description;
+    @NotNull
+    private double price;
+    @NotNull
+    private Integer stock;
+    @NotNull
+    private Date expiryDate;
+    @NotNull
+    private Integer quantity;
+    @NotNull
+    private SkinType skinType;
+
 }
