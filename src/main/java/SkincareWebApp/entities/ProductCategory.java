c48fee5f3f66d0f7eb9dcdd2924bf549caf8f976
= <joridalilollari27@gamil.com>
2024-05-31 19:41:14 +0200

    someChanges

--- src/main/java/SkincareWebApp/entities/ProductCategory.java
+++ src/main/java/SkincareWebApp/entities/ProductCategory.java
@@ -1,10 +1,12 @@
 package SkincareWebApp.entities;
 
 import jakarta.persistence.*;
+import lombok.Data;
 //import org.hibernate.annotations.Table;
 
 @Entity
 @Table(name = "ProductCategory")
+@Data
 public class ProductCategory {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -14,29 +16,5 @@ public class ProductCategory {
     private String categoryName;
     @Column(name = "ProductID", nullable = false)
     private Long productId;
-    // Constructors
-    public ProductCategory() {}
-    public ProductCategory(String categoryName, Long productId) {
-        this.categoryName = categoryName;
-        this.productId = productId;
-    }
-    // Getters and Setters
-    public Long getId() {
-        return id;
-    }
-    public void setId(Long id) {
-        this.id = id;
-    }
-    public String getCategoryName() {
-        return categoryName;
-    }
-    public void setCategoryName(String categoryName) {
-        this.categoryName = categoryName;
-    }
-    public Long getProductId() {
-        return productId;
-    }
-    public void setProductId(Long productId) {
-        this.productId = productId;
-    }
+
 }
--- src/main/java/SkincareWebApp/entities/UserOrder.java
+++ src/main/java/SkincareWebApp/entities/UserOrder.java
@@ -2,11 +2,13 @@ package SkincareWebApp.entities;
 
 
 import jakarta.persistence.*;
+import lombok.Data;
 
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 @Entity
 @Table(name = "UserOrder")
+@Data
 public class UserOrder {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -20,38 +22,6 @@ public class UserOrder {
     private BigDecimal totalAmount;
     @Column(name = "OrderDate", nullable = false)
     private LocalDateTime orderDate;
-    public UserOrder() {
-        this.orderDate = LocalDateTime.now();
-    }
-    // Getters and Setters
-    public Long getOrderId() {
-        return orderId;
-    }
-    public void setOrderId(Long orderId) {
-        this.orderId = orderId;
-    }
-    public Long getProductId() {
-        return productId;
-    }
-    public void setProductId(Long productId) {
-        this.productId = productId;
-    }
-    public Long getUserId() {
-        return userId;
-    }
-    public void setUserId(Long userId) {
-        this.userId = userId;
-    }
-    public BigDecimal getTotalAmount() {
-        return totalAmount;
-    }
-    public void setTotalAmount(BigDecimal totalAmount) {
-        this.totalAmount = totalAmount;
-    }
-    public LocalDateTime getOrderDate() {
-        return orderDate;
-    }
-    public void setOrderDate(LocalDateTime orderDate) {
-        this.orderDate = orderDate;
-    }
+
+
 }
